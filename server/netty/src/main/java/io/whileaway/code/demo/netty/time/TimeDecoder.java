0bdfda75c43fb278d5c482be15b220b053c89616
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-19 16:59:02 +0800

    update byte handle Solution 3

--- server/netty/src/main/java/io/whileaway/code/demo/netty/time/TimeDecoder.java
+++ server/netty/src/main/java/io/whileaway/code/demo/netty/time/TimeDecoder.java
@@ -2,17 +2,13 @@ package io.whileaway.code.demo.netty.time;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 
 import java.util.List;
 
-public class TimeDecoder extends ByteToMessageDecoder { // (1)
+public class TimeDecoder extends ReplayingDecoder<Void> {
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
-        if (in.readableBytes() < 4) {
-            return; // (3)
-        }
-
-        out.add(in.readBytes(4)); // (4)
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+        out.add(in.readBytes(4));
     }
 }
